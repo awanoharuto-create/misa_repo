bff066c17959c475824fbf352f517691edc1c6f2
baby <awanoharuto@gmail.com>
2026-08-20 16:08:39 +0900

    Replace OCR with free Pokedex image matching
    図鑑照合が、候補にすら上がらない。日本語のポケモン名を入れても出てこない

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'iv-scan-v2';
-const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest'];
+const CACHE = 'iv-scan-v3';
+const ASSETS = ['./', './index.html', './styles.css', './classifier.css', './app.js', './manifest.webmanifest'];
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
 self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))));
 self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(hit => hit || fetch(e.request))));
